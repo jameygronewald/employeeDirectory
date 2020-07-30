3e888220f38c76c7362eaf3fe184f72afe2f0c59
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-30 13:56:01 -0700

    created Directory and Employee components with basic scaffolding and improted css files to each

---
+++ src/components/Employee/Employee.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Employee.css';
+
+class Employee extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Employee;
