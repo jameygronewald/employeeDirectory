b63acf9a5311c8cb287b6cd90e3b7a949854fdc8
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-30 17:21:43 -0700

    added an input to App.js with some styling; set state property of filter to an empty string and made an onChange event handler with a handleChange method that sets state to whatever is input by user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import Directory from './components/Directory/Directory';
+import React from "react";
+import Directory from "./components/Directory/Directory";
+import "./App.css";
 
 class App extends React.Component {
+  state = {
+    filter: "",
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ filter: value });
+  };
+
   render() {
     return (
-      <div>
-        <Directory name='Jamey'/>
+      <div className="container">
+        <input
+          value={this.state.filter}
+          name="filter"
+          type="text"
+          placeholder="Filter by last name"
+          onChange={this.handleChange}
+        />
+        <Directory />
       </div>
     );
   }
--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -7,11 +7,11 @@ class Employee extends Component {
       <div className="row">
         <img src={this.props.picture.thumbnail} alt={this.props.name.first} />
         <h3>
-          Name: {this.props.name.first} {this.props.name.last}
+          {this.props.name.first} {this.props.name.last}
         </h3>
-        <h3>Phone: {this.props.phone}</h3>
-        <h3>Email: {this.props.email}</h3>
-        <h3>DOB: {this.props.dob.date}</h3>
+        <h3>{this.props.phone}</h3>
+        <h3>{this.props.email}</h3>
+        <h3>{this.props.dob.date}</h3>
       </div>
     );
   }
