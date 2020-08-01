5a6151d60b15fcd5f99b2f380ecee3a1f835afe3
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-01 16:24:46 -0700

    added utils directory w API.js for axios call to randomuser to generate employees from API call; imported into Directory component and setState to results of axios call at the time of component mount(when page loads); employees are dynamically rendering on page load and can be filtered by last name still

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import "./Directory.css";
 import Employee from "../Employee/Employee";
-import employees from "../../dummyData.json";
+import API from "../../utils/API";
 
 class Directory extends Component {
-  state = {
-    employees: employees,
-  };
+    
+    state = {
+      employees: [],
+    };
+
+    componentDidMount() {
+        API.getEmployees().then(({data: {results}}) => this.setState({employees: results})).catch(err => console.log(err))
+    };
+  
 
   render() {
-    const filteredEmployees = this.state.employees.filter(employee => this.props.filter.toLowerCase() === employee.name.last.slice(0, this.props.filter.length).toLowerCase());
+    const filteredEmployees = this.state.employees.filter(
+      employee =>
+        this.props.filter.toLowerCase() ===
+        employee.name.last.slice(0, this.props.filter.length).toLowerCase()
+    );
     return filteredEmployees.map((employee, index) => (
       <div>
         <Employee key={index + 1} {...employee} />
       </div>
-    ))
+    ));
   }
 }
 
