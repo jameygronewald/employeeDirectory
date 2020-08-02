92aa3fce881ece68c7bfc91c77bcca464751dfaf
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-01 17:03:01 -0700

    small styling fixes

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -4,22 +4,16 @@ import Employee from "../Employee/Employee";
 import API from "../../utils/API";
 
 class Directory extends Component {
-    
-    state = {
-      employees: [],
-    };
+  state = {
+    employees: [],
+  };
 
-    componentDidMount() {
-        API.getEmployees().then(({data: {results}}) => this.setState({employees: results})).catch(err => console.log(err))
-    };
-  
+  componentDidMount() {
+    API.getEmployees().then(({ data: { results } }) => this.setState({ employees: results })).catch(err => console.log(err));
+  };
 
   render() {
-    const filteredEmployees = this.state.employees.filter(
-      employee =>
-        this.props.filter.toLowerCase() ===
-        employee.name.last.slice(0, this.props.filter.length).toLowerCase()
-    );
+    const filteredEmployees = this.state.employees.filter(employee => this.props.filter.toLowerCase() === employee.name.last.slice(0, this.props.filter.length).toLowerCase());
     return filteredEmployees.map((employee, index) => (
       <div>
         <Employee key={index + 1} {...employee} />
