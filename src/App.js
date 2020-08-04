330d1ff07b8f212ddc01d1ec5fcf4335e3f2ed06
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-04 14:52:24 -0700

    converted components to functional components and used useEffect and useState hook; built a Form component and moved some of the code over to the Directory component from App.js to simplify the App.js page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Directory from "./components/Directory/Directory";
 import Navbar from "./components/Navbar/Navbar";
+import Form from "./components/Form/Form"
 import "./App.css";
 
-class App extends React.Component {
-  state = {
+function App() {
+  
+  const [state, setState] = useState({
     filter: "",
     sort: ""
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setState({ ...state, [name]: value });
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  render() {
     return (
       <div className="container">
         <Navbar />
-        <div className="searchRow">
-          <input
-            className="col"
-            value={this.state.filter}
-            name="filter"
-            type="text"
-            placeholder="Filter by last name"
-            onChange={this.handleChange}
-          />
-          <div className="col">
-            <label htmlFor="sorting">Sort by: </label>
-            <select name="sort" onChange={this.handleChange}>
-              <option value="">(none)</option>
-              <option value="name.last">Last Name (A-Z)</option>
-              <option value="email">Email (A-Z)</option>
-              <option value="dob.date">Date of Birth (ascending)</option>
-            </select>
-          </div>
-        </div>
-        <div className="tableHeaderRow">
-          <h2>Image</h2>
-          <h2>Name</h2>
-          <h2>Phone Number</h2>
-          <h2>Email</h2>
-          <h2>Date of Birth</h2>
-        </div>
-        <Directory filter={this.state.filter} sort={this.state.sort}/>
+        <Form handleChange={handleChange} filter={state.filter}/>
+        <Directory filter={state.filter} sort={state.sort}/>
       </div>
     );
-  }
 }
 
 export default App;
